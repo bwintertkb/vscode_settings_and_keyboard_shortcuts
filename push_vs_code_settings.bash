2b62f571f40cc82e9875d56c686afed3b3518874
Bartlomiej Winter <bartekwinter3@gmail.com>
2022-11-13 23:44:50 +0000

    Updated settings

---
+++ push_vs_code_settings.bash
@@ -0,0 +1,9 @@
+cp C:/Users/barte/AppData/Roaming/Code/User/keybindings.json C:/Users/barte/Documents/vscode_settings_and_keyboard_shortcuts/keybindings.json
+
+cp C:/Users/barte/AppData/Roaming/Code/User/settings.json C:/Users/barte/Documents/vscode_settings_and_keyboard_shortcuts/settings.json
+
+cd "C:/Users/barte/Documents/vscode_settings_and_keyboard_shortcuts"
+
+git add -A 
+git commit -a -m "Updated settings"
+git push
